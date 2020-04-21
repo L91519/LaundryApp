2deae5785aaa2fe202e4222d264afc0db29af802
Sumin <jennifer045020@gmail.com>
2020-04-21 22:54:20 +0900

    Delete Person Model

--- LaundryApp/app/src/main/java/com/example/laundryapp/base/BaseActivity.kt
+++ LaundryApp/app/src/main/java/com/example/laundryapp/base/BaseActivity.kt
@@ -15,7 +15,7 @@ abstract class BaseActivity<B : ViewDataBinding, VM : BaseViewModel>(
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-˚
+
         binding = DataBindingUtil.setContentView(this, layoutRes)
         binding.setVariable(BR.vm, vm)
         binding.lifecycleOwner = this
