667d9bf878a597ca5aa2c84218fc579766bbcb5a
Sumin <jennifer045020@gmail.com>
2020-04-23 22:36:42 +0900

    Edit Laundry List Recyclerview Adapter

--- LaundryApp/app/src/main/java/com/example/laundryapp/ui/laundry_list/LaundryListRecyclerViewAdapter.kt
+++ LaundryApp/app/src/main/java/com/example/laundryapp/ui/laundry_list/LaundryListRecyclerViewAdapter.kt
@@ -1,13 +1,17 @@
 package com.example.laundryapp.ui.laundry_list
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.example.laundryapp.R
 import com.example.laundryapp.data.model.LaundryModel
-import com.example.laundryapp.databinding.ActivityLaundryListBinding
 import com.example.laundryapp.databinding.ItemLaundryBinding
 
 class LaundryListRecyclerViewAdapter
     : RecyclerView.Adapter<LaundryListRecyclerViewAdapter.LaundryListViewHolder>() {
+    private val items = mutableListOf<LaundryModel>()
+    private lateinit var binding: ItemLaundryBinding
 
     inner class LaundryListViewHolder(private val binding: ItemLaundryBinding)
         : RecyclerView.ViewHolder(binding.root){
@@ -18,19 +22,25 @@ class LaundryListRecyclerViewAdapter
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LaundryListViewHolder {
-        TODO("Not yet implemented")
+        binding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.item_laundry,
+            parent,
+            false
+        )
+        return LaundryListViewHolder(binding)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = items.size
 
     override fun onBindViewHolder(holder: LaundryListViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(items[position])
     }
 
-    fun setItems(){
-
+    fun setItems(newItems: MutableList<LaundryModel>){
+        items.clear()
+        items.addAll(newItems)
+        notifyDataSetChanged()
     }
 
 }
