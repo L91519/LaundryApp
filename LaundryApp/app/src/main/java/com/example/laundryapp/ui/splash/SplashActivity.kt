48a89159fb3c8cd79e95229fa2aa2946bc8c1198
Sumin <jennifer045020@gmail.com>
2020-04-28 20:44:51 +0900

    Edit Splash Activity

--- LaundryApp/app/src/main/java/com/example/laundryapp/ui/splash/SplashActivity.kt
+++ LaundryApp/app/src/main/java/com/example/laundryapp/ui/splash/SplashActivity.kt
@@ -1,5 +1,22 @@
 package com.example.laundryapp.ui.splash
 
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import com.example.laundryapp.R
+import com.example.laundryapp.ui.laundry_list.LaundryListActivity
 
-class SplashActivity: AppCompatActivity()
+class SplashActivity: AppCompatActivity() {
+    val SPLASH_TIME: Long = 1500
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, LaundryListActivity::class.java))
+            finish()
+        }, SPLASH_TIME)
+    }
+}
