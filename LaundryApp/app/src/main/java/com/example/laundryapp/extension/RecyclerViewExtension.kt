3ada3625cf27119f784a61d0847b39797f49e7da
Sumin <jennifer045020@gmail.com>
2020-04-28 20:46:00 +0900

    Edit RecyclerView Extension

---
+++ LaundryApp/app/src/main/java/com/example/laundryapp/extension/RecyclerViewExtension.kt
@@ -0,0 +1,15 @@
+package com.example.laundryapp.extension
+
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.laundryapp.data.model.LaundryModel
+import com.example.laundryapp.ui.laundry_list.LaundryListRecyclerViewAdapter
+
+@BindingAdapter("bindLaundryItems")
+fun RecyclerView.bindLaundryItems(items: MutableList<LaundryModel>, showAll: Boolean){
+    if(adapter is LaundryListRecyclerViewAdapter) {
+        items?.let {
+            (adapter as LaundryListRecyclerViewAdapter).setItems(items, showAll)
+        }
+    }
+}
