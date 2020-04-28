7e5128419cd1a09097bcc9bd7739c01f70eb584f
Sumin <jennifer045020@gmail.com>
2020-04-28 20:47:26 +0900

    Add DataBinding

--- LaundryApp/app/src/main/java/com/example/laundryapp/ui/laundry_list/LaundryListRecyclerViewAdapter.kt
+++ LaundryApp/app/src/main/java/com/example/laundryapp/ui/laundry_list/LaundryListRecyclerViewAdapter.kt
@@ -1,17 +1,20 @@
 package com.example.laundryapp.ui.laundry_list
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.laundryapp.R
 import com.example.laundryapp.data.model.LaundryModel
 import com.example.laundryapp.databinding.ItemLaundryBinding
+import kotlinx.android.synthetic.main.item_order.view.*
 
 class LaundryListRecyclerViewAdapter
     : RecyclerView.Adapter<LaundryListRecyclerViewAdapter.LaundryListViewHolder>() {
-    private val items = mutableListOf<LaundryModel>()
+    private var items = mutableListOf<LaundryModel>()
     private lateinit var binding: ItemLaundryBinding
+    private var showAllLaundry = false
 
     inner class LaundryListViewHolder(private val binding: ItemLaundryBinding)
         : RecyclerView.ViewHolder(binding.root){
@@ -19,6 +22,16 @@ class LaundryListRecyclerViewAdapter
         fun bind(laundryItem: LaundryModel){
             binding.item = laundryItem
         }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position != OWNER_VISIBLE) {
+            if (items[position - 1].owner != items[position].owner) {
+                OWNER_VISIBLE
+            } else
+                OWNER_GONE
+        } else
+            OWNER_VISIBLE
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LaundryListViewHolder {
@@ -31,16 +44,33 @@ class LaundryListRecyclerViewAdapter
         return LaundryListViewHolder(binding)
     }
 
-    override fun getItemCount(): Int = items.size
+    override fun getItemCount(): Int {
+
+        return if(showAllLaundry) items.size
+        else items.size
+    }
 
     override fun onBindViewHolder(holder: LaundryListViewHolder, position: Int) {
+        if (holder.itemViewType == OWNER_VISIBLE)
+            holder.itemView.rl_customer_info.visibility = View.VISIBLE
+        else if (holder.itemViewType == OWNER_GONE)
+            holder.itemView.rl_customer_info.visibility = View.GONE
+
         holder.bind(items[position])
     }
 
-    fun setItems(newItems: MutableList<LaundryModel>){
+    fun setItems(newItems: MutableList<LaundryModel>, showAll: Boolean){
         items.clear()
-        items.addAll(newItems)
+        if (showAll)
+            items.addAll(newItems)
+        else
+            items.addAll(newItems.filter { !it.isDone })
         notifyDataSetChanged()
+    }
+
+    companion object {
+        const val OWNER_VISIBLE: Int = 0
+        const val OWNER_GONE: Int = 1
     }
 
 }
