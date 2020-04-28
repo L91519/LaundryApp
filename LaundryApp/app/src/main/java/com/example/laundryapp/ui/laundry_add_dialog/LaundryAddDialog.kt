e25431217814fdbf4ddd9b3417f3d019c38a1efa
Sumin <jennifer045020@gmail.com>
2020-04-28 20:47:55 +0900

    Create Laundry Add Dialog File

---
+++ LaundryApp/app/src/main/java/com/example/laundryapp/ui/laundry_add_dialog/LaundryAddDialog.kt
@@ -0,0 +1,7 @@
+package com.example.laundryapp.ui.laundry_add_dialog
+
+import androidx.fragment.app.DialogFragment
+
+class LaundryAddDialog : DialogFragment() {
+
+}
---
+++ LaundryApp/app/src/main/java/com/example/laundryapp/ui/laundry_add_dialog/LaundryAddDialogViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.laundryapp.ui.laundry_add_dialog
+
+import com.example.laundryapp.base.BaseViewModel
+
+class LaundryAddDialogViewModel : BaseViewModel() {
+
+}
