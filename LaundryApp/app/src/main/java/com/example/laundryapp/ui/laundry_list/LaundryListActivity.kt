6e0b97ca7033e20142cdfc7b444743d751ff1ebc
Sumin <jennifer045020@gmail.com>
2020-04-28 20:47:02 +0900

    Add RecyclerView Adapter

--- LaundryApp/app/src/main/java/com/example/laundryapp/ui/laundry_list/LaundryListActivity.kt
+++ LaundryApp/app/src/main/java/com/example/laundryapp/ui/laundry_list/LaundryListActivity.kt
@@ -1,11 +1,34 @@
 package com.example.laundryapp.ui.laundry_list
 
+import android.os.Bundle
+import androidx.lifecycle.Observer
 import com.example.laundryapp.R
 import com.example.laundryapp.base.BaseActivity
 import com.example.laundryapp.databinding.ActivityLaundryListBinding
+import kotlinx.android.synthetic.main.activity_laundry_list.*
 
 class LaundryListActivity :
     BaseActivity<ActivityLaundryListBinding, LaundryListViewModel>(R.layout.activity_laundry_list){
     override val vm: LaundryListViewModel =
         LaundryListViewModel()
+    private lateinit var adapter: LaundryListRecyclerViewAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        adapter = LaundryListRecyclerViewAdapter()
+        binding.rvLaundryList.adapter = adapter
+
+        observableProperty()
+    }
+
+    private fun observableProperty(){
+        vm.observableFinishedLaundryShow.observe(this@LaundryListActivity, Observer {
+
+        })
+
+        vm.observableShowDialog.observe(this@LaundryListActivity, Observer {
+
+        })
+    }
 }
--- LaundryApp/app/src/main/java/com/example/laundryapp/ui/laundry_list/LaundryListViewModel.kt
+++ LaundryApp/app/src/main/java/com/example/laundryapp/ui/laundry_list/LaundryListViewModel.kt
@@ -1,5 +1,30 @@
 package com.example.laundryapp.ui.laundry_list
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.laundryapp.base.BaseViewModel
+import com.example.laundryapp.data.model.LaundryModel
 
-class LaundryListViewModel : BaseViewModel()
+class LaundryListViewModel : BaseViewModel() {
+
+    private val _observableLaundryList = MutableLiveData<MutableList<LaundryModel>>()
+    val observableLaundryList : LiveData<MutableList<LaundryModel>>
+        get() = _observableLaundryList
+
+    private val _observableShowDialog = MutableLiveData(false)
+    val observableShowDialog : LiveData<Boolean>
+        get() = _observableShowDialog
+
+    private val _observableFinishedLaundryShow = MutableLiveData(false)
+    val observableFinishedLaundryShow : LiveData<Boolean>
+        get() = _observableFinishedLaundryShow
+
+    fun showLaundryAddDialog() {
+        _observableShowDialog.value = true
+    }
+
+    fun finishedLaundryFilter() {
+        _observableFinishedLaundryShow.value = true
+    }
+
+}
