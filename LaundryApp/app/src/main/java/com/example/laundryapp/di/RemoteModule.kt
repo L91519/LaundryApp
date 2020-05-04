7b68dcdd7b5ace6dd93f5f83cd266f9807f82fba
Sumin <jennifer045020@gmail.com>
2020-05-04 23:28:48 +0900

    Create Remote Module di

--- LaundryApp/app/src/main/java/com/example/laundryapp/di/RemoteModule.kt
+++ LaundryApp/app/src/main/java/com/example/laundryapp/di/RemoteModule.kt
@@ -1,5 +1,41 @@
 package com.example.laundryapp.di
 
+import com.example.laundryapp.data.source.remote.LaundryRemoteDataSourceImpl
+import com.example.laundryapp.data.source.remote.network.LaundryApi
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
-val remoteModule = module {  }
+private const val BASE_URL = "https://openapi.naver.com/v1/"
+private const val TIMEOUT: Long = 15
+
+val remoteModule = module {
+
+    factory {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        OkHttpClient().newBuilder()
+            .connectTimeout(TIMEOUT, TimeUnit.SECONDS)
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
+
+    single {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(get<OkHttpClient>())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    single {
+        get<Retrofit>().create(LaundryApi::class.java)
+    }
+
+    single {
+        LaundryRemoteDataSourceImpl(get())
+    }
+}
