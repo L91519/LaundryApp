79f9ac8ec7fc756e8e510294c0c5ca29173235d7
Sumin <jennifer045020@gmail.com>
2020-05-06 22:32:59 +0900

    Add Laundry List ViewModel Function

--- LaundryApp/app/src/main/java/com/example/laundryapp/ui/laundry_add_dialog/LaundryAddDialogViewModel.kt
+++ LaundryApp/app/src/main/java/com/example/laundryapp/ui/laundry_add_dialog/LaundryAddDialogViewModel.kt
@@ -1,7 +1,9 @@
 package com.example.laundryapp.ui.laundry_add_dialog
 
 import com.example.laundryapp.base.BaseViewModel
+import com.example.laundryapp.data.repository.LaundryAppRepository
 
-class LaundryAddDialogViewModel : BaseViewModel() {
+class LaundryAddDialogViewModel constructor(private val repository: LaundryAppRepository) :
+    BaseViewModel() {
 
 }
--- LaundryApp/app/src/main/java/com/example/laundryapp/ui/laundry_list/LaundryListViewModel.kt
+++ LaundryApp/app/src/main/java/com/example/laundryapp/ui/laundry_list/LaundryListViewModel.kt
@@ -4,8 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.laundryapp.base.BaseViewModel
 import com.example.laundryapp.data.model.LaundryModel
+import com.example.laundryapp.data.repository.LaundryAppRepository
 
-class LaundryListViewModel : BaseViewModel() {
+class LaundryListViewModel constructor(private val repository: LaundryAppRepository) :
+    BaseViewModel() {
 
     private val _observableLaundryList = MutableLiveData<MutableList<LaundryModel>>()
     val observableLaundryList : LiveData<MutableList<LaundryModel>>
@@ -25,6 +27,71 @@ class LaundryListViewModel : BaseViewModel() {
 
     fun finishedLaundryFilter() {
         _observableFinishedLaundryShow.value = true
+    }
+
+    fun getRecentLaundries() {
+        repository.getRecentLaundries(
+            success = {},
+            fail = {}
+        )
+    }
+
+    fun saveRecentLaundries() {
+        //TODO : Using Rx might help
+    }
+
+    fun getLaundries() {
+        repository.getLaundries(
+            success = {},
+            fail = {}
+        )
+    }
+
+    fun getLaundriesByBrand(brand: String) {
+        repository.getLaundriesByBrand(
+            brand,
+            success = {},
+            fail = {}
+        )
+    }
+
+    fun getLaundriesByKind(kind: String) {
+        repository.getLaundriesByKind(
+            kind,
+            success = {},
+            fail = {}
+        )
+    }
+
+    fun getLaundriesByOwner(owner: String) {
+        repository.getLaundriesByOwner(
+            owner,
+            success = {},
+            fail = {}
+        )
+    }
+
+    fun getLaundriesByPH(ph: String) {
+        repository.getLaundriesByPH(
+            ph,
+            success = {},
+            fail = {}
+        )
+    }
+
+    fun delLaundries(laundry: LaundryModel) {
+        repository.delLaundry(
+            laundry,
+            success = {},
+            fail = {})
+    }
+
+    fun saveLaundries(laundry: LaundryModel) {
+        repository.saveLaundry(
+            laundry,
+            success = {},
+            fail = {}
+        )
     }
 
 }
