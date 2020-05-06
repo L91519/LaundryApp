2463df40dd927580225d4c661cd83c5331bc6b18
Sumin <jennifer045020@gmail.com>
2020-05-06 22:30:36 +0900

    Use ViewModel Di

--- LaundryApp/app/src/main/java/com/example/laundryapp/di/ViewModelModule.kt
+++ LaundryApp/app/src/main/java/com/example/laundryapp/di/ViewModelModule.kt
@@ -1,7 +1,16 @@
 package com.example.laundryapp.di
 
+import com.example.laundryapp.ui.laundry_add_dialog.LaundryAddDialogViewModel
+import com.example.laundryapp.ui.laundry_list.LaundryListViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
+    viewModel {
+        LaundryListViewModel(get())
+    }
 
+    viewModel {
+        LaundryAddDialogViewModel(get())
+    }
 }
--- LaundryApp/app/src/main/java/com/example/laundryapp/ui/laundry_list/LaundryListActivity.kt
+++ LaundryApp/app/src/main/java/com/example/laundryapp/ui/laundry_list/LaundryListActivity.kt
@@ -6,11 +6,12 @@ import com.example.laundryapp.R
 import com.example.laundryapp.base.BaseActivity
 import com.example.laundryapp.databinding.ActivityLaundryListBinding
 import kotlinx.android.synthetic.main.activity_laundry_list.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class LaundryListActivity :
     BaseActivity<ActivityLaundryListBinding, LaundryListViewModel>(R.layout.activity_laundry_list){
-    override val vm: LaundryListViewModel =
-        LaundryListViewModel()
+    override val vm by viewModel<LaundryListViewModel>()
+
     private lateinit var adapter: LaundryListRecyclerViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
