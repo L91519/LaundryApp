832ec181527c382923967162143c2853635e08cf
Sumin <jennifer045020@gmail.com>
2020-05-06 22:31:21 +0900

    Edit Laundry Api

--- LaundryApp/app/src/main/java/com/example/laundryapp/data/source/remote/network/LaundryApi.kt
+++ LaundryApp/app/src/main/java/com/example/laundryapp/data/source/remote/network/LaundryApi.kt
@@ -2,4 +2,17 @@ package com.example.laundryapp.data.source.remote.network
 
 interface LaundryApi{
 
+    fun getLaundries()
+
+    fun getLaundriesByBrand()
+
+    fun getLaundriesByOwner()
+
+    fun getLaundriesByPH()
+
+    fun getLaundriesByKind()
+
+    fun delLaundry()
+
+    fun saveLaundry()
 }
