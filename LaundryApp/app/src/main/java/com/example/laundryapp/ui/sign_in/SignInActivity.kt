07c663f4744ef830d574c3e63a1080f521abdb23
Sumin <jennifer045020@gmail.com>
2020-05-11 23:48:57 +0900

    Create Sign In, Sign Up Activity

---
+++ LaundryApp/app/src/main/java/com/example/laundryapp/ui/sign_in/SignInActivity.kt
@@ -0,0 +1,11 @@
+package com.example.laundryapp.ui.sign_in
+
+import com.example.laundryapp.R
+import com.example.laundryapp.base.BaseActivity
+import com.example.laundryapp.databinding.ActivitySignInBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class SignInActivity :
+    BaseActivity<ActivitySignInBinding, SignInViewModel>(R.layout.activity_sign_in) {
+    override val vm: SignInViewModel by viewModel<SignInViewModel>()
+}
---
+++ LaundryApp/app/src/main/java/com/example/laundryapp/ui/sign_up/SignUpViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.laundryapp.ui.sign_up
+
+import com.example.laundryapp.base.BaseViewModel
+
+class SignUpViewModel : BaseViewModel() {}
