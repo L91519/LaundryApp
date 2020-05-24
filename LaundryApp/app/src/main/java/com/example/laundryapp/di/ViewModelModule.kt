ad4cd66897220161dc1ac88f8503fd9cd55f6ff5
Sumin <jennifer045020@gmail.com>
2020-05-24 23:35:28 +0900

    Create Firebase Package

--- LaundryApp/app/src/main/java/com/example/laundryapp/di/ViewModelModule.kt
+++ LaundryApp/app/src/main/java/com/example/laundryapp/di/ViewModelModule.kt
@@ -2,8 +2,8 @@ package com.example.laundryapp.di
 
 import com.example.laundryapp.ui.laundry_add_dialog.LaundryAddDialogViewModel
 import com.example.laundryapp.ui.laundry_list.LaundryListViewModel
-import com.example.laundryapp.ui.sign_in.SignInViewModel
-import com.example.laundryapp.ui.sign_up.SignUpViewModel
+import com.example.laundryapp.ui.auth.sign_in.SignInViewModel
+import com.example.laundryapp.ui.auth.sign_up.SignUpViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
