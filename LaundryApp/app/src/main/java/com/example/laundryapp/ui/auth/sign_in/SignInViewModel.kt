dce29a53aab3f3e38d189382556a695e2c314e63
Sumin <jennifer045020@gmail.com>
2020-05-27 23:48:11 +0900

    Change File Location

--- LaundryApp/app/src/main/java/com/example/laundryapp/ui/auth/sign_in/SignInViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.laundryapp.ui.auth.sign_in
-
-import com.example.laundryapp.base.BaseViewModel
-
-class SignInViewModel : BaseViewModel() {
-
-}
--- LaundryApp/app/src/main/java/com/example/laundryapp/ui/auth/sign_up/SignUpViewModel.kt
+++
@@ -1,5 +0,0 @@
-package com.example.laundryapp.ui.auth.sign_up
-
-import com.example.laundryapp.base.BaseViewModel
-
-class SignUpViewModel : BaseViewModel() {}
