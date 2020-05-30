48938de343944ec4112cbf2f85d984e69afd1e24
Sumin <jennifer045020@gmail.com>
2020-05-30 22:55:37 +0900

    Add Laundry Add Dialog VM, V

--- LaundryApp/app/src/main/java/com/example/laundryapp/ui/laundry_add_dialog/LaundryAddDialog.kt
+++ LaundryApp/app/src/main/java/com/example/laundryapp/ui/laundry_add_dialog/LaundryAddDialog.kt
@@ -1,7 +1,57 @@
 package com.example.laundryapp.ui.laundry_add_dialog
 
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
+import com.example.laundryapp.BR
+import com.example.laundryapp.R
+import com.example.laundryapp.databinding.DialogAddLaundryBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class LaundryAddDialog : DialogFragment() {
+    private val vm by viewModel<LaundryAddDialogViewModel>()
 
+    private lateinit var binding: DialogAddLaundryBinding
+    private lateinit var _context: Context
+
+    private lateinit var adapter: LaundryAddRecyclerViewAdapter
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        _context = context
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val dialogWidth = _context.resources.getDimensionPixelSize(R.dimen.laundry_add_dialog_width)
+        val dialogHeight = _context.resources.getDimensionPixelSize(R.dimen.laundry_add_dialog_height)
+        dialog?.window?.setLayout(dialogWidth, dialogHeight)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: DialogAddLaundryBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(context),
+            R.layout.dialog_add_laundry,
+            null,
+            false
+        )
+        binding.setVariable(BR.vm, vm)
+        binding.lifecycleOwner = this
+
+        adapter = LaundryAddRecyclerViewAdapter()
+        binding.rvLaundryAddList.adapter = adapter
+
+        return binding.root
+    }
 }
--- LaundryApp/app/src/main/java/com/example/laundryapp/ui/laundry_add_dialog/LaundryAddDialogViewModel.kt
+++ LaundryApp/app/src/main/java/com/example/laundryapp/ui/laundry_add_dialog/LaundryAddDialogViewModel.kt
@@ -1,9 +1,54 @@
 package com.example.laundryapp.ui.laundry_add_dialog
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.laundryapp.base.BaseViewModel
-import com.example.laundryapp.data.repository.LaundryAppRepository
+import com.example.laundryapp.data.firebase.repository.FirebaseRepository
+import com.example.laundryapp.data.model.OrderModel
 
-class LaundryAddDialogViewModel constructor(private val repository: LaundryAppRepository) :
+class LaundryAddDialogViewModel constructor(private val repository: FirebaseRepository) :
     BaseViewModel() {
+
+    private val _observableOrderList = MutableLiveData<MutableList<OrderModel>>()
+    val observableOrderList: LiveData<MutableList<OrderModel>>
+        get() = _observableOrderList
+
+    private val _observableToast = MutableLiveData<String>()
+    val observableToast: LiveData<String>
+        get() = _observableToast
+
+    val observableOrderBrand = MutableLiveData<String>()
+    val observableOrderKind = MutableLiveData<String>()
+    val observableOrderPrice = MutableLiveData<String>()
+
+    private val tmpItems = mutableListOf<OrderModel>()
+
+    fun dismissDialog() {
+
+    }
+
+    fun addLaundries() {
+
+    }
+
+    fun addOrder() {
+//        _observableOrderList.value?.add(OrderModel("","",""))
+        if (observableOrderBrand.value.isNullOrEmpty() ||
+            observableOrderKind.value.isNullOrEmpty() ||
+            observableOrderPrice.value.isNullOrEmpty()
+        ) {
+            _observableToast.value = "Fill in the blank"
+            return
+        } else {
+            tmpItems.add(
+                OrderModel(
+                    observableOrderBrand.value,
+                    observableOrderKind.value,
+                    observableOrderPrice.value
+                )
+            )
+            _observableOrderList.value = tmpItems
+        }
+    }
 
 }
