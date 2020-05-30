9a6b39faeea19bf423cd56d480fa5e97cf054603
Sumin <jennifer045020@gmail.com>
2020-05-30 22:51:58 +0900

    Add Firebase Module

---
+++ LaundryApp/app/src/main/java/com/example/laundryapp/di/FirebaseModule.kt
@@ -0,0 +1,22 @@
+package com.example.laundryapp.di
+
+import com.example.laundryapp.data.firebase.source.FirebaseSource
+import com.example.laundryapp.data.firebase.source.FirebaseSourceImpl
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import org.koin.dsl.module
+
+val firebaseModule = module {
+
+    single {
+        FirebaseDatabase.getInstance()
+    }
+
+    single {
+        FirebaseAuth.getInstance()
+    }
+
+    single<FirebaseSource> {
+        FirebaseSourceImpl()
+    }
+}
