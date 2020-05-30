27e4e5f80f49311a2f8b986677823493aefb71df
Sumin <jennifer045020@gmail.com>
2020-05-30 22:58:11 +0900

    Change Laundry Activity to Auth Activity

--- LaundryApp/app/src/main/java/com/example/laundryapp/ui/splash/SplashActivity.kt
+++ LaundryApp/app/src/main/java/com/example/laundryapp/ui/splash/SplashActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.example.laundryapp.R
-import com.example.laundryapp.ui.laundry_list.LaundryListActivity
+import com.example.laundryapp.ui.auth.SignInActivity
 
 class SplashActivity: AppCompatActivity() {
     val SPLASH_TIME: Long = 1500
@@ -15,7 +15,7 @@ class SplashActivity: AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         Handler().postDelayed({
-            startActivity(Intent(this, LaundryListActivity::class.java))
+            startActivity(Intent(this, SignInActivity::class.java))
             finish()
         }, SPLASH_TIME)
     }
