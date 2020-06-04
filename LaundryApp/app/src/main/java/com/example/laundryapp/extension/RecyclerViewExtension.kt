0c53f1ee8c4dae0e8bbb95794e5fd599b05b835e
Sumin <jennifer045020@gmail.com>
2020-06-04 18:01:36 +0900

    Edit Binding Adapter. Two Param -> One Param

--- LaundryApp/app/src/main/java/com/example/laundryapp/extension/RecyclerViewExtension.kt
+++ LaundryApp/app/src/main/java/com/example/laundryapp/extension/RecyclerViewExtension.kt
@@ -4,23 +4,32 @@ import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.laundryapp.data.model.LaundryModel
 import com.example.laundryapp.data.model.OrderModel
-import com.example.laundryapp.ui.laundry_add_dialog.LaundryAddRecyclerViewAdapter
+import com.example.laundryapp.ui.laundry_list.laundry_add_dialog.LaundryListAddRecyclerViewAdapter
 import com.example.laundryapp.ui.laundry_list.LaundryListRecyclerViewAdapter
 
-@BindingAdapter("bindLaundryItems", "bindLaundryShowAll")
-fun RecyclerView.bindLaundryItems(items: MutableList<LaundryModel>?, showAll: Boolean){
+//@BindingAdapter("bindLaundryItems", "bindLaundryShowAll")
+//fun RecyclerView.bindLaundryItems(items: MutableList<LaundryModel>?, showAll: Boolean){
+//    if(adapter is LaundryListRecyclerViewAdapter) {
+//        items?.let {
+//            (adapter as LaundryListRecyclerViewAdapter).setItems(items, showAll)
+//        }
+//    }
+//}
+
+@BindingAdapter("bindLaundryItems")
+fun RecyclerView.bindLaundryItems(items: MutableList<LaundryModel>?){
     if(adapter is LaundryListRecyclerViewAdapter) {
         items?.let {
-            (adapter as LaundryListRecyclerViewAdapter).setItems(items, showAll)
+            (adapter as LaundryListRecyclerViewAdapter).setItems(items)
         }
     }
 }
 
 @BindingAdapter("bindOrderItems")
 fun RecyclerView.bindOrderItems(items: MutableList<OrderModel>?) {
-    if (adapter is LaundryAddRecyclerViewAdapter) {
+    if (adapter is LaundryListAddRecyclerViewAdapter) {
         items?.let {
-            (adapter as LaundryAddRecyclerViewAdapter).setItems(items)
+            (adapter as LaundryListAddRecyclerViewAdapter).setItems(items)
         }
     }
 }
