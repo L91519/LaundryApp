d1723137ba27ba6b3dba4a58bed1c9e4e50ddbd5
Sumin <jennifer045020@gmail.com>
2020-06-10 23:25:04 +0900

    Add View Model

--- LaundryApp/app/src/main/java/com/example/laundryapp/di/ViewModelModule.kt
+++ LaundryApp/app/src/main/java/com/example/laundryapp/di/ViewModelModule.kt
@@ -2,6 +2,7 @@ package com.example.laundryapp.di
 
 import com.example.laundryapp.ui.auth.AuthViewModel
 import com.example.laundryapp.ui.laundry_list.LaundryListViewModel
+import com.example.laundryapp.ui.laundry_list.laundry_add_dialog.LaundryListAddViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -12,5 +13,9 @@ val viewModelModule = module {
 
     viewModel {
         LaundryListViewModel(get())
+    }
+
+    viewModel {
+        LaundryListAddViewModel(get())
     }
 }
