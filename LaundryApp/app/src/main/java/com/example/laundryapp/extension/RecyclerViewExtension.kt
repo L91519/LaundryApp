a7e95a8795efccaddc96f29d8e07c65e826b6a16
Sumin <jennifer045020@gmail.com>
2020-06-21 01:12:03 +0900

    Add Show All Param to Laundry List SetItems

--- LaundryApp/app/src/main/java/com/example/laundryapp/extension/RecyclerViewExtension.kt
+++ LaundryApp/app/src/main/java/com/example/laundryapp/extension/RecyclerViewExtension.kt
@@ -8,23 +8,23 @@ import com.example.laundryapp.data.model.OrderModel
 import com.example.laundryapp.ui.laundry_list.laundry_add_dialog.LaundryListAddRecyclerViewAdapter
 import com.example.laundryapp.ui.laundry_list.LaundryListRecyclerViewAdapter
 
-//@BindingAdapter("bindLaundryItems", "bindLaundryShowAll")
-//fun RecyclerView.bindLaundryItems(items: MutableList<LaundryModel>?, showAll: Boolean){
-//    if(adapter is LaundryListRecyclerViewAdapter) {
-//        items?.let {
-//            (adapter as LaundryListRecyclerViewAdapter).setItems(items, showAll)
-//        }
-//    }
-//}
-
-@BindingAdapter("bindLaundryItems")
-fun RecyclerView.bindLaundryItems(items: MutableList<LaundryModel>?){
+@BindingAdapter("bindLaundryItems", "bindLaundryShowAll")
+fun RecyclerView.bindLaundryItems(items: MutableList<LaundryModel>?, showAll: Boolean){
     if(adapter is LaundryListRecyclerViewAdapter) {
         items?.let {
-            (adapter as LaundryListRecyclerViewAdapter).setItems(items)
+            (adapter as LaundryListRecyclerViewAdapter).setItems(items, showAll)
         }
     }
 }
+
+//@BindingAdapter("bindLaundryItems")
+//fun RecyclerView.bindLaundryItems(items: MutableList<LaundryModel>?){
+//    if(adapter is LaundryListRecyclerViewAdapter) {
+//        items?.let {
+//            (adapter as LaundryListRecyclerViewAdapter).setItems(items)
+//        }
+//    }
+//}
 
 @BindingAdapter("bindOrderItems")
 fun RecyclerView.bindOrderItems(items: MutableList<OrderModel>?) {
